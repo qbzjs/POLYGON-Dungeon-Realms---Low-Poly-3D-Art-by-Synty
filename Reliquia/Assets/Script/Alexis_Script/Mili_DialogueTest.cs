e25092bd8ae43967324e36eb7407b0a493813930
Alexis60100 <alexis210496@hotmail.fr>
2021-04-25 19:06:22 +0200

    Update Mili 2/2
    Temp Dialogue System OK
    LD/3D Fusionnée

---
+++ Reliquia/Assets/Script/Alexis_Script/Mili_DialogueTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Mili_DialogueTest : MonoBehaviour{
+
+    public Text dialogueTest;
+
+    public string[] dialoguesTestMili;
+    public string selectedString;
+
+    public float[] textTimeDisplay; 
+
+
+    void OnTriggerEnter(Collider c){
+
+        StartCoroutine(DialogueTrigger());
+
+        if(c.gameObject.name == "Dialogue Trigger 1"){
+            selectedString = dialoguesTestMili[0];
+            dialogueTest.text = selectedString;
+        }
+
+        // Continue here
+
+    }
+
+    IEnumerator DialogueTrigger(){
+        dialogueTest.gameObject.SetActive(true);
+        yield return new WaitForSeconds (textTimeDisplay[0]);
+        dialogueTest.gameObject.SetActive(false);
+    }
+
+
+}
