f45ad5fdefcf3ae9179e414324b106d62e07337b
Alexis60100 <alexis210496@hotmail.fr>
2021-05-02 16:37:04 +0200

    Mili Dial/Lights

--- Reliquia/Assets/Script/Alexis_Script/Mili_DialogueTest.cs
+++ Reliquia/Assets/Script/Alexis_Script/Mili_DialogueTest.cs
@@ -12,31 +12,41 @@ public class Mili_DialogueTest : MonoBehaviour{
 
     public float[] textTimeDisplay; 
 
+    public bool textWasDisplayed1;
+    public bool textWasDisplayed2;
+    public bool textWasDisplayed3;
+
 
     void OnTriggerEnter(Collider c){
 
-        StartCoroutine(DialogueTrigger());
+        
 
-        if(c.gameObject.name == "Dialogue Trigger 1"){
+        if(c.gameObject.name == "Dialogue Trigger 1" && textWasDisplayed1 == false){
+            textWasDisplayed1 = true;
             selectedString = dialoguesTestMili[0];
             dialogueTest.text = selectedString;
+            StartCoroutine(DialogueTrigger());
         }
 
         // Continue here
 
-        if (c.gameObject.name == "Dialogue Trigger 2")
+        if (c.gameObject.name == "Dialogue Trigger 2" && textWasDisplayed2 == false)
         {
+            textWasDisplayed2 = true;
             selectedString = dialoguesTestMili[1];
             dialogueTest.text = selectedString;
+            StartCoroutine(DialogueTrigger());
         }
 
-        if (c.gameObject.name == "Dialogue Trigger 3")
+        if (c.gameObject.name == "Dialogue Trigger 3" && textWasDisplayed3 == false)
         {
+            textWasDisplayed3 = true;
             selectedString = dialoguesTestMili[2];
             dialogueTest.text = selectedString;
+            StartCoroutine(DialogueTrigger());
         }
 
-
+        
 
 
     }
