844a23f7e93df368754769f1fb151c5771858817
Loky <loky.jason@gmail.com>
2021-05-08 17:30:55 +0200

    Intégration Theme Menu
    Ajout de la musique dans la Scène Menu_01

---
+++ Reliquia/Assets/Script/Paul_Script/SoundManager_paul.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class SoundManager_paul : MonoBehaviour
+{
+    public AudioClip[] playlist;
+    public AudioSource audioSource;
+    private int musicIndex = 0;
+
+
+    // J'apppel mon Array
+    void Start()
+    {
+        audioSource.clip = playlist[0];
+        audioSource.Play();
+
+    }
+
+    void Update()
+    {
+        if (!audioSource.isPlaying){
+            PlayNextSong();
+        }
+    }
+
+    void PlayNextSong()
+    {
+        musicIndex = (musicIndex + 1) % playlist.Length;
+        audioSource.clip = playlist[musicIndex];
+        audioSource.Play();
+    }
+}
