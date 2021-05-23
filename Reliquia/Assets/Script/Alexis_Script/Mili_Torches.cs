fa156da38a989bbd33fc5110bbaca19ae69eb67e
Alexis60100 <alexis210496@hotmail.fr>
2021-05-23 20:04:27 +0200

    Mili Objets + Script Torches WIP

---
+++ Reliquia/Assets/Script/Alexis_Script/Mili_Torches.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mili_Torches : MonoBehaviour{
+
+    //public lightingScript;
+
+    public Transform[] torches;
+
+    public float distanceTolerance = 2f;
+
+    // Update is called once per frame
+    void Update(){
+
+        
+        foreach (var torche in torches){
+
+            // Calcul Distance
+
+            float dist = Vector3.Distance(torche.position, transform.position);
+            print("Distance to other: " + dist);
+
+
+            // Distance d'une torche
+
+            if(dist > distanceTolerance){
+  
+            }
+        }
+
+        
+
+        
+
+        
+        
+    }
+
+}
