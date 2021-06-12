3042b96bd0568792bada384cd0e2930f5e9fe9a4
Alexandre Venet <alexandrevenet@users.noreply.github.com>
2021-06-12 17:46:35 +0200

    Alexandre > Dialogue : CSVDispatcher
    Outil accessible en barre de menu Tools pour générer un fichier SO de dialogue à partir d'un tableur au format CSV (copier/coller de texte).

--- Reliquia/Assets/Editor/Alexandre_editor/Help/EHelpMenu.cs
+++ Reliquia/Assets/Editor/Alexandre_editor/Help/EHelpMenu.cs
@@ -8,22 +8,28 @@ namespace AlexandreDialogues
 {
 	public class EHelpMenu
 	{
-		[MenuItem("Tools/Reliquia/Dialogue/Help file")]
+		[MenuItem("Tools/Reliquia/Dialogue/Help file", false, 0)]
 		private static void NestedOption1()
 		{
 			Selection.activeObject = AssetDatabase.LoadMainAssetAtPath("Assets/Editor/Alexandre_editor/Help/HelpFile.asset"); ;
 		}
 
-		[MenuItem("Tools/Reliquia/Dialogue/Notice")]
+		[MenuItem("Tools/Reliquia/Dialogue/Notice", false, 1)]
 		private static void NestedOption2()
 		{
 			Selection.activeObject = AssetDatabase.LoadMainAssetAtPath("Assets/Editor/Alexandre_editor/Help/Dialogue system - notice.pdf"); ;
 		}
 
-		[MenuItem("Tools/Reliquia/Dialogue/Documentation")]
+		[MenuItem("Tools/Reliquia/Dialogue/Documentation", false, 2)]
 		private static void NestedOption3()
 		{
 			Selection.activeObject = AssetDatabase.LoadMainAssetAtPath("Assets/Editor/Alexandre_editor/Help/Dialogue system - documentation.pdf"); ;
+		}
+
+		[MenuItem("Tools/Reliquia/Dialogue/CSV Dispatcher", false, 14)] 
+		public static void CVSDispatcher()
+		{
+			ECSVDispatcher.ShowWindow();
 		}
 	}
 }
