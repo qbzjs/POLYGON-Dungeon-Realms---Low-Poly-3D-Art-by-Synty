f3e513ad22d86ba63113b37ab2b51e9cce99e8b4
Alexandre Venet <alexandrevenet@users.noreply.github.com>
2021-06-20 14:40:07 +0200

    Alexandre > Dialogue : composant StepTrigger
    Composant de gestion de déclenchement d'un InGame Dialogue à partir d'un ScriptableObject LevelProgress et d'un objet taggé.

---
+++ Reliquia/Assets/Script/Alexandre_Script/Game/InGameDialogue/InGameDialogueStepTrigger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AlexandreDialogues
+{
+	public class InGameDialogueStepTrigger : MonoBehaviour
+	{
+		#region Private
+
+		[Tooltip("Un fichier ScriptableObject de dialogue in-game.")]
+		[SerializeField] private InGameDialogue _inGameDialogue;
+		[Tooltip("Un fichier ScriptableObject de progression.")]
+		[SerializeField] private LevelProgress _levelProgress;
+		[Tooltip("La valeur à laquelle lancer le dialogue.")]
+		[SerializeField] private int _value;
+		[Tooltip("Le tag d'objet à tester lors du OnTriggerEnter")]
+		[SerializeField] private string _tag;
+
+		#endregion
+
+
+
+		#region Frame cycle
+
+		private void OnTriggerEnter(Collider other)
+		{
+			if (_levelProgress.m_levelStep == _value)
+			{
+				if (other.CompareTag(_tag))
+				{
+					InGameDialogueManager.Instance.StartDialogue(_inGameDialogue);
+					_levelProgress.m_levelStep++;
+				}
+			}
+		}
+
+		#endregion
+	}
+}
