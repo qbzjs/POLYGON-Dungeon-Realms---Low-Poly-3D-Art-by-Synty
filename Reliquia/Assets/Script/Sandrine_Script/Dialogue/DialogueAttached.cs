d149be3becd71bfcbeaa2bff3bfe519d4564b240
sandoproj <71790005+sandoproj@users.noreply.github.com>
2021-08-30 16:21:31 +0200

    Test Intégration Dialogues LD_Catacombes

---
+++ Reliquia/Assets/Script/Sandrine_Script/Dialogue/DialogueAttached.cs
@@ -0,0 +1,14 @@
+﻿using AlexandreDialogues;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueAttached : MonoBehaviour
+{
+    public InGameDialogue inGameDialogue;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        InGameDialogueManager.Instance.StartDialogue(inGameDialogue);
+    }
+}
