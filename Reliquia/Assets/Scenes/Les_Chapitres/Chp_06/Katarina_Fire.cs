2525446a791f8f9522ac076134120cfe36fe1799
Alexis60100 <47276845+Alexis60100@users.noreply.github.com>
2021-12-05 16:55:03 +0100

    Laby + Katarina
    Laby :
    - Gardes Golems
    - Ajout Garde
    - Structure Tunnels
    - Gestion Murs
    - Objets avec Materials
    
    Alchimiste :
    - Katarina Active

---
+++ Reliquia/Assets/Scenes/Les_Chapitres/Chp_06/Katarina_Fire.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Katarina_Fire : MonoBehaviour{
+
+    public Transform player;
+    public float lifetime = 3f;
+
+    void Start(){
+        player = GameObject.Find("ybot").GetComponent<Transform>();
+        DestroyObjectDelayed();
+    }
+
+    void Update(){
+        transform.position = Vector3.MoveTowards(transform.position, player.position, 0.5f);
+    }
+
+    void OnCollisionEnter(Collision c){
+        Destroy(this.gameObject);
+    }
+    
+
+    void DestroyObjectDelayed(){
+        Destroy(this.gameObject, lifetime);
+    }
+}
---
+++ Reliquia/Assets/Script/Alexis_Script/Katarina.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Katarina : MonoBehaviour{
+
+    public GameObject fireBall;
+    public Transform player;
+    public float cooldownTimer;
+    public float cooldown = 3f;
+
+    void Start(){
+        player = GameObject.Find("ybot").GetComponent<Transform>();
+    }
+
+    void Update(){
+
+        transform.LookAt(player);
+
+        if(cooldownTimer >= 0){
+            cooldownTimer -= 1 * Time.deltaTime;
+        }
+
+        if(cooldownTimer <= 0){
+            Instantiate(fireBall, new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, this.gameObject.transform.position.z), Quaternion.identity);
+            cooldownTimer = cooldown;
+        }
+        
+    }
+}
