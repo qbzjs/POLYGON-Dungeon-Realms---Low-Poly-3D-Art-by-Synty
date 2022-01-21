4daaf14081f527581bf465be8d1cd6e23c9f8af2
Alexis60100 <47276845+Alexis60100@users.noreply.github.com>
2022-01-21 15:54:26 +0100

    LD Laby
    - Lumières
    - Pièce 3
    - Terre
    - Trigger Box Crevasses/Eau

---
+++ Reliquia/Assets/Script/Alexis_Script/WaterDeath.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterDeath : MonoBehaviour{
+
+    // Ce script gère les crevasses dy Labyrinthe, il doit être placé sur le Player
+
+}
