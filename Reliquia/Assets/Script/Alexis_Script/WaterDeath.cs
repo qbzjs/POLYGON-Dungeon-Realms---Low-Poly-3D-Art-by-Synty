08b8b6122a5e0c2bac980e1fcf498dcd59e54de4
Alexis60100 <47276845+Alexis60100@users.noreply.github.com>
2022-02-01 17:54:32 +0100

    Laby : Mort Crevasse + Dynamite/C4

--- Reliquia/Assets/Script/Alexis_Script/WaterDeath.cs
+++ Reliquia/Assets/Script/Alexis_Script/WaterDeath.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class WaterDeath : MonoBehaviour{
 
+
     // Ce script gère les crevasses dy Labyrinthe, il doit être placé sur le Player
+
+    void OnTriggerEnter(Collider c) {
+        if (c.gameObject.name == "WaterDeathZone"){
+         this.gameObject.transform.position = new Vector3 (0, 0, 0);  
+        }
+    }
 
 }
