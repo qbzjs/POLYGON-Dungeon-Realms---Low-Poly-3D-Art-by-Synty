35da309b3b95e1f286b91a6fb795fdc3c9499984
VinceCDA <vincent.hitman47@hotmail.fr>
2022-02-11 18:07:23 +0100

    Mise à jour interaction et ajout du système de mana
    -Ajout du script Mana.cs
    -Ajout Sprite pour William, Roxane et David
    -Modifier script DialogueAttached.cs pour fonctionner avec ThirdPersonSystem
    -Ajout DialogueManager et InGameDialogueManager à la scène BaseMilitaire
    -Ajout de dialogue et de ingame dialogue à la BaseMilitaire
    -Ajout SoundManager à la scène BaseMilitaire pour pouvoir lancer les interactions.

--- Reliquia/Assets/Script/Sandrine_Script/Dialogue/DialogueAttached.cs
+++ Reliquia/Assets/Script/Sandrine_Script/Dialogue/DialogueAttached.cs
@@ -1,4 +1,6 @@
 ﻿using AlexandreDialogues;
+using Cinemachine;
+using DiasGames.ThirdPersonSystem;
 using System.Collections;
 using System.Collections.Generic;
 using System.Security.Permissions;
@@ -9,6 +11,8 @@ public class DialogueAttached : MonoBehaviour
     public InGameDialogue inGameDialogue;
     public Dialogue dialogue;
     public GameObject virtualCamera;
+    public CinemachineStateDrivenCamera cinemachineStateDriven;
+    private ThirdPersonSystem thirdPersonSystem;
 
     private bool alreadyRed;
     public bool showOnSecondThrow;
@@ -24,6 +28,13 @@ public class DialogueAttached : MonoBehaviour
             // Lancer un dialogue Dialogue Box 
             if (dialogue != null)
             {
+                if (other.TryGetComponent<ThirdPersonSystem>(out thirdPersonSystem))
+                {
+                    ForceIdleAnimation();
+                    JoueurPeutBouger(false);
+                    StartCoroutine(CheckDialogueFin());
+                }
+
                 DialogueManager.Instance.StartDialogueFromFile(dialogue, virtualCamera, true, true);
             }
             else
@@ -41,5 +52,40 @@ public class DialogueAttached : MonoBehaviour
         {
             showOnSecondThrow = false;
         }
+    }
+    private IEnumerator CheckDialogueFin()
+    {
+        yield return new WaitForSeconds(0.1f);
+        Debug.Log("Couroutine Runnig");
+        if (!DialogueManager.Instance.IsDialogueStarted && alreadyRed)
+        {
+            if (thirdPersonSystem != null)
+            {
+                JoueurPeutBouger(true);
+            }
+        }
+        else
+        {
+            thirdPersonSystem.m_Rigidbody.velocity = Vector3.zero;
+            StartCoroutine(CheckDialogueFin());
+        }
+    }
+    private void ForceIdleAnimation()
+    {
+        foreach (AnimatorControllerParameter parameter in thirdPersonSystem.m_Animator.parameters)
+        {
+            thirdPersonSystem.m_Animator.SetFloat(parameter.name, 0);
+        }
+    }
+    private void JoueurPeutBouger(bool etat)
+    {
+
+        cinemachineStateDriven.enabled = etat;
+        foreach (ThirdPersonAbility ability in thirdPersonSystem.CharacterAbilities)
+        {
+            ability.enabled = etat;
+        }
+
+        thirdPersonSystem.enabled = etat;
     }
 }
---
+++ Reliquia/Assets/Script/Vincent_Script/Mana.cs
@@ -0,0 +1,79 @@
+﻿using DiasGames.ThirdPersonSystem;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Mana : Modifier
+{
+    [SerializeField] private float m_MaxMana = 100f;
+
+
+    //[Header("Audio Clips")]
+    //[SerializeField] private AudioClip m_HurtClip = null;
+
+
+    [Header("Events")]
+    [SerializeField] private UnityEvent OnManaDamage;
+
+    // Action Events
+    public Action OnCharacterManaDamage, OnManaChanged;
+
+    private float m_CurrentMana;
+
+
+    public float ManaValue { get { return m_CurrentMana; } }
+    public float MaximumMana { get { return m_MaxMana; } }
+
+    //private CharacterAudioManager m_AudioManager;
+    private float m_LastDamagedTime = 0;
+
+    private void Awake()
+    {
+
+        //m_AudioManager = GetComponent<CharacterAudioManager>();
+
+        m_CurrentMana = m_MaxMana;
+
+        GlobalEvents.AddEvent("ManaDamage", ManaDamage);
+        GlobalEvents.AddEvent("RestoreMana", RestoreMana);
+
+    }
+
+    private void RestoreMana(GameObject obj, object value)
+    {
+        m_CurrentMana += (float)value;
+        m_CurrentMana = Mathf.Clamp(m_CurrentMana, 0, m_MaxMana);
+        OnManaChanged?.Invoke();
+    }
+
+    private void ManaDamage(GameObject obj, object amount)
+    {
+        if (m_LastDamagedTime > Time.fixedTime)
+            return;
+
+        if (obj != gameObject)
+        {
+            return;
+        }
+
+        m_CurrentMana -= (float)amount;
+        if (m_CurrentMana <= 0)
+        {
+            m_CurrentMana = 0;
+            return;
+        }
+        else
+        {
+            OnManaDamage.Invoke();
+            OnCharacterManaDamage?.Invoke();
+            //if (m_HurtClip != null && m_AudioManager != null)
+            //    m_AudioManager.PlayVoiceSound(m_HurtClip);
+        }
+
+        m_LastDamagedTime = Time.fixedTime + 0.1f;
+        OnManaChanged?.Invoke();
+    }
+
+}
