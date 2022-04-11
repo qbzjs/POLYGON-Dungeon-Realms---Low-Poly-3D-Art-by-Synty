00a52e2f3c52de8a1ceeb975bae29b6e6f3d6ae3
VinceCDA <vincent.hitman47@hotmail.fr>
2022-04-11 11:20:36 +0200

    Correction bugs dialogue box et PouvoirLighting
    -modification script DialogueManager pour SlotsPouvoirs
    -modification script PourvoirLighting
    -ajout tag "Viseur" et "SlotsPouvoirs"
    -modification prefab HUD

--- Reliquia/Assets/Script/Vincent_Script/PouvoirLighting.cs
+++ Reliquia/Assets/Script/Vincent_Script/PouvoirLighting.cs
@@ -37,6 +37,7 @@ public class PouvoirLighting : ThirdPersonAbility,IPouvoir
     public override void Initialize(ThirdPersonSystem mainSystem, AnimatorManager animatorManager, UnityInputManager inputManager)
     {
         base.Initialize(mainSystem, animatorManager, inputManager);
+        _viseur = GameObject.FindGameObjectWithTag("Viseur");
         _lightingOrbe.SetActive(false);
         _viseur.SetActive(false);
     }
@@ -83,6 +84,10 @@ public class PouvoirLighting : ThirdPersonAbility,IPouvoir
     {
         base.OnExitAbility();
         _lightingOrbe.SetActive(false);
+        if (_viseur.activeSelf)
+        {
+            _viseur.SetActive(false);
+        }
         StopCoroutine(_coroutineDrainMana);
     }
     /// <summary>
