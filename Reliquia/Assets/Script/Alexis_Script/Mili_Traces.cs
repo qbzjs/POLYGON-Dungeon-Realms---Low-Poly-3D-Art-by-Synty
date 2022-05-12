8eacc7cd578bcc7b8d313d09705fbe01202b6319
Alexis60100 <47276845+Alexis60100@users.noreply.github.com>
2022-05-12 17:01:07 +0200

    LD : Mili + Tuto Update
    Mili : Traces de Pas + 1er Script
    Tuto : Level Design/FBX

---
+++ Reliquia/Assets/Script/Alexis_Script/Mili_Traces.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mili_Traces : MonoBehaviour{
+
+    public Lighting_Vincent_Test lightingScript;
+   
+    void Start(){
+        lightingScript = GameObject.Find("William_Player").GetComponent<Lighting_Vincent_Test>();
+    }
+
+    void Update(){
+        if(lightingScript.isCreated == false){
+            this.gameObject.SetActive(false);
+            Debug.Log("Inactive");
+        }
+
+        if(lightingScript.isCreated == true){
+            this.gameObject.SetActive(true);
+            Debug.Log("Active");
+        }
+
+        Debug.Log(lightingScript.isCreated);
+    }
+}
