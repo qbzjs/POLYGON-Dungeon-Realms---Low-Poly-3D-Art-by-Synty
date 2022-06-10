dd30a3c0920ee24f14536b115e3e2c4b734cfadd
Hellfii <56402063+Hellfii@users.noreply.github.com>
2022-06-10 17:01:39 +0200

    Debugging
    - Fix des inputs
    - Ajout d'un deathplane
    - Réajustements des vitesses crouch et pouvoir

---
+++ Reliquia/Assets/Script/Yann_Script/DeathPlane.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DiasGames.ThirdPersonSystem;
+using UnityEngine;
+
+public class DeathPlane : MonoBehaviour
+{
+    private Animator playerAnim;
+    private Health _thirdPersonSystem;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && other.GetComponent<Animator>() != null)
+        {
+            //playerAnim = other.GetComponent<Animator>();
+
+            //PlayerDeath();
+            _thirdPersonSystem = other.GetComponent<Health>();
+            _thirdPersonSystem.Die();
+
+
+        }
+    }
+
+    private void PlayerDeath()
+    {
+        if (playerAnim == null)
+        {
+            return;
+        }
+
+        StartCoroutine(ReLoad());
+
+
+    }
+
+    private IEnumerator ReLoad()
+    {
+
+        float countWait = 5f;
+        yield return new WaitForSeconds(countWait);
+        ReLoadCheckpoint();
+    }
+
+    private void ReLoadCheckpoint()
+    {
+        string nom = GameManager.instance.nomSauvegarde;
+        if (nom != null && SaveManager.instance != null)
+        {
+            bool retrunToCheckPoint = true;
+            //InventaireSauvegarde.instance.ResetSessionInventaire();
+            SaveManager.instance.LoadInGame(nom, retrunToCheckPoint);
+        }
+
+    }
+
+
+}
