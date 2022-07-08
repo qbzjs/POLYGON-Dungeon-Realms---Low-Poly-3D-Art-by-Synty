6daa9456fa82181bc2d123e7d57ac49aa3e3e143
Hellfii <56402063+Hellfii@users.noreply.github.com>
2022-07-08 17:24:35 +0200

    QTE GrayscaleFade + RegenHealth
    Ajout du grayscalefade sur le qte et ajout d'une régénération de vie sur le script Health.

--- Reliquia/Assets/Script/Yann_Script/PPGrayscale.cs
+++ Reliquia/Assets/Script/Yann_Script/PPGrayscale.cs
@@ -11,10 +11,6 @@ public class PPGrayscale : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // _material = new Material(_shader);
-       // rend = GetComponent<Renderer>();
-
-        // Use the Specular shader on the material
         rend.material.shader = Shader.Find("Blend");
         _material.SetFloat("_Blend", 0f);
 
@@ -23,12 +19,6 @@ public class PPGrayscale : MonoBehaviour
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
         Graphics.Blit(source, destination, _material);
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        _material.SetFloat("_Blend", 0f);
-
     }
 
     void Update()
