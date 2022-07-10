c3cc4668467697a2902303ab4159e84d9455f605
Elye <rikougo.kagakusha@gmail.com>
2022-07-10 14:06:43 +0200

    Curseur personalisé draft

---
+++ Reliquia/Assets/Script/Samuel_Script/CurseurControlleur.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurseurControlleur : MonoBehaviour
+{
+    public Texture2D curseurClassique, curseurCroix, curseurMain;
+
+    private CurseurControlleur sInstance;
+
+    public CurseurControlleur Instance { get; private set; }
+
+    void Awake() {
+        if (Instance != null) {
+            Destroy(this);
+            return;
+        }
+
+        Instance = this;
+
+        Cursor.SetCursor(curseurClassique, Vector2.up + Vector2.left, CursorMode.Auto);
+        DontDestroyOnLoad(this);
+    }
+}
