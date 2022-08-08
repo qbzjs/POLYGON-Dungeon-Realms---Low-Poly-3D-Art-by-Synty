f43219a47f897e8471ace79670bda600e226dab2
Hellfii <56402063+Hellfii@users.noreply.github.com>
2022-08-08 17:00:46 +0200

    Katarina Phase 1 + 2
    Ajout Rig Katarina
    Ajout ColliderBurn sur les murs
    Ajout Hitbox sur Pulsate
    Ajout DestroyCylinder en Phase 2
    Ajout Dialogues (temporairement set false)

---
+++ Reliquia/Assets/Script/Yann_Script/DestroyOnCollider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnCollider : MonoBehaviour
+{
+    public GameObject Cylinder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Phase2"))
+        {
+            Destroy(Cylinder);
+        }
+    }
+}
