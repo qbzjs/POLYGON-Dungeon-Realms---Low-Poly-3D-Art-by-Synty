db6c10590c72eae4b0502c57c38cdac799bab82b
Alexis60100 <47276845+Alexis60100@users.noreply.github.com>
2022-08-23 00:21:17 +0200

    LD Tuto : Juicy
    - Flèches mobiles avec vitesse éditable
    - Piège de Feu faisant retourner au début
    - Barrières ouvertes avec animations (Elles ne se ferment pas encore d'elles-mêmes)
    - Particules avec feuilles dans le vent

---
+++ Reliquia/Assets/Script/Alexis_Script/ArrowBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowBehaviour : MonoBehaviour{
+
+    //Transform transform;
+    Vector3 startPosition;
+    Rigidbody rigid;
+
+    public float speed = 1f; 
+
+    void Start(){
+        startPosition = this.transform.position;
+        rigid = this.GetComponent<Rigidbody>();
+    }
+
+    void Update(){
+        rigid.velocity = transform.forward * speed;
+    }
+
+    void OnCollisionEnter(){
+        this.transform.position = startPosition;
+    }
+}
