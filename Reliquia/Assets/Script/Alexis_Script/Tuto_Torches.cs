964416dfeb4b6f3bb62196f309488100b5e3af13
Alexis60100 <47276845+Alexis60100@users.noreply.github.com>
2022-10-05 00:04:03 +0200

    Tuto + Pouvoirs
    - Tuto : FX Leaves + Caméra Blocks + Scaling
    - Pouvoirs : Lighting Shot 2->1

--- Reliquia/Assets/Script/Alexis_Script/Tuto_Torches.cs
+++ Reliquia/Assets/Script/Alexis_Script/Tuto_Torches.cs
@@ -6,7 +6,6 @@ public class Tuto_Torches : MonoBehaviour{
 
     [Header("Torches")]
 
-    
     public GameObject[] torches;
     public bool[] activated;
 
@@ -27,32 +26,75 @@ public class Tuto_Torches : MonoBehaviour{
     [Header("Right Path")]
 
     public GameObject[] cubes;
+    public Camera cameraCubes;
+    public bool cameraCubesEnabled = false;
+    public float[] cubesScaleY = new float[3];
 
-    public void OnTriggerEnter(Collider c){
+    void Update(){
+        if(cameraTimer > 0){
+            cameraTimer -= 1 * Time.deltaTime;
+        }
 
-        if(c.gameObject.name == torches[0].name){
+        if(cameraTimer <= 0){
+            cameraTimer = cameraTime;
+            for (int i = 0; i < portalsCameras.Length; i++){
+                portalsCameras[i].GetComponent<Camera>().enabled = false;
+            }
+            
+        }
+
+        //for (int i = 0; i < torchesSparks.Length; i++){
+        if(torchesSparks[0].activeSelf && torchesSparks[1].activeSelf && torchesSparks[2].activeSelf && torchesSparks[3].activeSelf && torchesSparks[4].activeSelf && 
+            torchesSparks[5].activeSelf){
+            portalEnd.gameObject.SetActive(true);
+        }
+        //}
+
+        if(portalEnd.gameObject.activeSelf){
+            portalEnd.transform.LookAt(this.transform.position);
+            portalEnd.transform.eulerAngles = new Vector3 (0, portalEnd.transform.eulerAngles.y, 0);
+
+        }
+
+        if(torches[0].gameObject.activeSelf){
             torchesSparks[0].gameObject.SetActive(true);
             torchesSparks[1].gameObject.SetActive(true);
             portalsTriggers[0].gameObject.SetActive(true);
             activated[0] = true;
         }
 
-        if(c.gameObject.name == torches[1].name){
+        if(torches[1].gameObject.activeSelf){
             torchesSparks[2].gameObject.SetActive(true);
             torchesSparks[3].gameObject.SetActive(true);
             portalsTriggers[1].gameObject.SetActive(true);
             activated[1] = true;
         }
 
-        if(c.gameObject.name == torches[2].name){
+        if(torches[2].gameObject.activeSelf){
             torchesSparks[4].gameObject.SetActive(true);
             torchesSparks[5].gameObject.SetActive(true);
             portalsTriggers[2].gameObject.SetActive(true);
-            cubes[0].transform.localScale = new Vector3(this.transform.localScale.x, 1.3f, this.transform.localScale.z);
-            cubes[1].transform.localScale = new Vector3(this.transform.localScale.x, 1.55f, this.transform.localScale.z);
-            cubes[2].transform.localScale = new Vector3(this.transform.localScale.x, 1.8f, this.transform.localScale.z);
             activated[2] = true;
+            
+            cameraCubes.GetComponent<Camera>().enabled = true;
+
+            if(cubes[0].transform.localScale.y < cubesScaleY[0]){
+                cubes[0].transform.localScale += new Vector3(0, 0.5f, 0) * Time.deltaTime;
+            }
+            if(cubes[1].transform.localScale.y < cubesScaleY[1]){
+                cubes[1].transform.localScale += new Vector3(0, 0.5f, 0) * Time.deltaTime;
+            }
+            if(cubes[2].transform.localScale.y < cubesScaleY[2]){
+                cubes[2].transform.localScale += new Vector3(0, 0.5f, 0) * Time.deltaTime;
+            }
+
+            if(cubes[2].transform.localScale.y >= cubesScaleY[2]){
+                cameraCubes.GetComponent<Camera>().enabled = false;
+            }
         }
+    }
+
+    public void OnTriggerEnter(Collider c){
 
         if(c.gameObject.name == portalsTriggers[0].name && activated[0] == true && camerasActivated[0] == false){
             camerasActivated[0] = true;
@@ -64,6 +106,7 @@ public class Tuto_Torches : MonoBehaviour{
 
         if(c.gameObject.name == portalsTriggers[1].name && activated[1] == true && camerasActivated[1] == false){
             camerasActivated[1] = true;
+            cameraTimer = cameraTime;
             portalsCameras[1].GetComponent<Camera>().enabled = true;
             portals[2].SetTrigger("Close");
             portals[3].SetTrigger("Close");
@@ -71,6 +114,7 @@ public class Tuto_Torches : MonoBehaviour{
 
         if(c.gameObject.name == portalsTriggers[2].name && activated[2] == true && camerasActivated[2] == false){
             camerasActivated[2] = true;
+            cameraTimer = cameraTime;
             portalsCameras[2].GetComponent<Camera>().enabled = true;
             portals[4].SetTrigger("Close");
             portals[5].SetTrigger("Close");
@@ -97,32 +141,7 @@ public class Tuto_Torches : MonoBehaviour{
 
     }
 
-    void Update(){
-        if(cameraTimer > 0){
-            cameraTimer -= 1 * Time.deltaTime;
-        }
-
-        if(cameraTimer <= 0){
-            cameraTimer = cameraTime;
-
-            for (int i = 0; i < portalsCameras.Length; i++){
-                portalsCameras[i].GetComponent<Camera>().enabled = false;
-            }
-        }
-
-        //for (int i = 0; i < torchesSparks.Length; i++){
-        if(torchesSparks[0].activeSelf && torchesSparks[1].activeSelf && torchesSparks[2].activeSelf && torchesSparks[3].activeSelf && torchesSparks[4].activeSelf && 
-            torchesSparks[5].activeSelf){
-            portalEnd.gameObject.SetActive(true);
-        }
-        //}
-
-        if(portalEnd.gameObject.activeSelf){
-            portalEnd.transform.LookAt(this.transform.position);
-            portalEnd.transform.eulerAngles = new Vector3 (0, portalEnd.transform.eulerAngles.y, 0);
-
-        }
-    }
+    
 
 }
 
